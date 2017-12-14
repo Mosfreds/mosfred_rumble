1e2ffa6b3654e67af006dc7b49c72f76fa451699
Frauke <foe@offis.de>
2017-12-14 14:53:34 +0100

    create ann method

--- vindinium/bots/mosfred_rumble.py
+++ vindinium/bots/mosfred_rumble.py
@@ -1,9 +1,12 @@
 import os
-
+import numpy as np
 from vindinium.bots import BaseBot
 from vindinium.ai import Minimax
-
+from keras.layers import Dense, Activation, Flatten
+from keras.optimizers import Adam
+from keras.models import Sequential
 from keras.models import model_from_json
+from observations.action_space import VindiniumMoveSpace
 
 
 
@@ -12,6 +15,7 @@ __all__ = ['MosfredRumble']
 
 class MosfredRumble(BaseBot):
     _ann = None
+
 
     def serialize_ann(self, path='.', base_name='mosfred_rumble'):
         model = _ann.to_json()
@@ -23,3 +27,15 @@ class MosfredRumble(BaseBot):
         with open("{}/{}.json".format(path, base_name), 'r') as json_file:
             _ann = model_from_json(json_file.read())
         _ann.load_weights("{}/{}.h5".format(path, base_name))
+
+
+    def create_ann(self):
+        model = Sequential()
+        num_actions = len(VindiniumMoveSpace.get_moves())
+#        model.add(Flatten(input_shape=(1,) + gamestate.shape))
+        model.add(Dense(500, input_dim=50, activation='relu'))
+        model.add(Dense(500, activation='relu'))
+        # model.add(Activation('relu'))
+        model.add(Dense(num_actions, activation='linear'))
+        return model
+
