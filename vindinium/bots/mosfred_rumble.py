b1144ab277bfbc28ab91525e164d349ad8cfdb94
Eric MSP Veith <eric.veith@offis.de>
2017-12-14 13:57:47 +0100

    Requirements file, ANN serialize/deseralize

--- vindinium/bots/mosfred_rumble.py
+++ vindinium/bots/mosfred_rumble.py
@@ -1,9 +1,25 @@
+import os
+
 from vindinium.bots import BaseBot
 from vindinium.ai import Minimax
+
+from keras.models import model_from_json
+
 
 
 __all__ = ['MosfredRumble']
 
 
 class MosfredRumble(BaseBot):
-    pass
+    _ann = None
+
+    def serialize_ann(self, path='.', base_name='mosfred_rumble'):
+        model = _ann.to_json()
+        with open("{}/{}.json".format(path, base_name), 'w') as json_file:
+            json_file.write(model)
+        _ann.save_weights("{}/{}.h5".format(path, base_name))
+
+    def deserialize_ann(self, path='.', base_name='mosfred_rumble'):
+        with open("{}/{}.json".format(path, base_name), 'r') as json_file:
+            _ann = model_from_json(json_file.read())
+        _ann.load_weights("{}/{}.h5".format(path, base_name))
