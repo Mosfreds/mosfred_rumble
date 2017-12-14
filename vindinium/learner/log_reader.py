387e9c582e0974b9f52c1b11ee6607a33aca0bb2
Eric MSP Veith <eric.veith@offis.de>
2017-12-14 15:32:11 +0100

    Fixed LogReader iterator

--- vindinium/learner/log_reader.py
+++ vindinium/learner/log_reader.py
@@ -4,17 +4,17 @@ import json
 
 class LogReader:
     def __init__(self, io_Obj):
-        self.file = io_Obj
+        self._file = io_Obj
         
     def __iter__(self):
         return self
 
     def __next__(self):
-        l = self.file.readline()
+        l = self._file.readline()
         if not l:
             raise StopIteration
         while not l.startswith("data: "):
-               l = readline(self.file)
-        state = {'game':json.load(l[6:])}
+           l = self._file.readline()
+        state = {'game':json.loads(l[6:])}
         return game.Game(state)
 
