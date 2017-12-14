25270ecd59b383cf1526e80cd5ca7b052040b3e5
Lars Fischer <fischer@wiwi.uni-siegen.de>
2017-12-14 15:07:45 +0100

    untested gamestate reader

---
+++ vindinium/learner/log.py
@@ -0,0 +1,24 @@
+
+from vindinium.models import game 
+import json
+
+class LogReader:
+    def __init__(self, io_Obj):
+        self.file = io_Obj
+        
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        l = self.file.readline()
+        if not l:
+            raise StopIteration
+        while not l.startswith("data: "):
+               l = readline(self.file)
+        state = {'game':json.load(l[6:])}
+        return game.Game(state)
+        
+
+
+#if __name__ == "__main__":
+    
